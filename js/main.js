cc99286dac33d13f306e5dfc5e4a9e96102c62dd
devcarolinealmeida <carolineitalia85@gmail.com>
2022-07-30 12:35:08 +0200

    inserindo comando botao reset

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
-
-
-
-
-
-// logica: ao clicar no botao da porcentagem, capturar o botao escolhido e calcular o juros. 100(valor) * (10% de 100) porcentagem do valor. valor * (porcentagem/100)
-
-// pegar valor do botao de juros escolhido, só pode ser um
-
-// pegar o input das parcelas
-// dividir valor com juros pela qtde de parcelas
-// mostrar valor cada parcela display
-// mostrar valor total com juros
-
 const inputValor = document.querySelector("[data-valor]")
 const buttonPorcentagem = document.querySelectorAll("[data-porcentagem]")
 const inputParcelas = document.querySelector("[data-parcelas]")
 const displayParcela = document.querySelector("[data-p]")
 const displayTotal = document.querySelector("[data-v]")
+const buttonReset = document.getElementById("recalcular")
 
 // o que sera executado qdo apertar botao porcentagem
 buttonPorcentagem.forEach((elemento) => {
@@ -29,14 +16,20 @@ buttonPorcentagem.forEach((elemento) => {
             const valorDivido = valorComJuros/nParcela
             displayParcela.textContent = valorDivido.toFixed(2)
             displayTotal.textContent = valorComJuros.toFixed(2)
-            //atualizar o display ja com os calculos feitos
-            
         })
     })
-    
+    buttonReset.addEventListener("click", () => {
+        inputValor.value = ""
+        inputParcelas.value = ""
+        displayParcela.textContent = "00.00"
+        displayTotal.textContent = "00.00"
+    })
 })
 
 
+
+
+    
 
 
 function calculaPorcentagem(porcentagem, valor) {
